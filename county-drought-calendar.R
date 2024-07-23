a494da9d9e81eb92e22480e299e2a01b97ac5b86
Kyle Bocinsky <bocinsky@gmail.com>
2024-07-23 14:25:56 -0600

    simplified plotting

--- county-drought-calendar.R
+++ county-drought-calendar.R
@@ -17,10 +17,16 @@ fsa_normal_grazing_periods <-
   )
 
 county_drought_calendar <-
-  function(county, years){
+  function(county = "30063", 
+           years = 2000:lubridate::year(today()), 
+           width = 8){
     
-    # years <- 2020:2024
+    # years <- 2000:2024
     # county <- "30063"
+    
+    
+    
+    
     
     calendar_data <-
       usdm_counties %>%
@@ -55,213 +61,249 @@ county_drought_calendar <-
                       factor(., levels = 0:53, labels = 1:54, ordered = TRUE)
       ) %>%
       dplyr::ungroup() %>%
-      dplyr::filter(Date <= today()) %>%
-      dplyr::filter(Year %in% years)
+      dplyr::filter(Date <= today())
     
-    month_breaks <-
-      calendar_data %>%
-      dplyr::filter(lubridate::mday(Date) == 1,
-                    Month != "Jan")
-    
-    # lab_breaks <-
-    #   calendar_data %>%
-    #   dplyr::filter(lubridate::mday(Date) == 1,
-    #                 Year == min(Year)) %>%
-    #   dplyr::mutate(Week = ifelse(wday != 1, as.integer(Week) + 1, Week),
-    #                 Week = ifelse(Month == "Jan", 1, Week)) %$%
-    #   Week
-    
-    lab_breaks <-
-      c(Jan = 3,
-        Feb = 7,
-        Mar = 11,
-        Apr = 15,
-        May = 20,
-        Jun = 24,
-        Jul = 29,
-        Aug = 33,
-        Sep = 38,
-        Oct = 42,
-        Nov = 46,
-        Dec = 51
-      )
-    
-    lw = 0.8
-    
-    p <- 
-      calendar_data %>%
-      # tail(20)
-      ggplot() +
-      geom_tile(aes(x = Week, 
-                    y = Day, 
-                    fill = `USDM Class`),
-                color = "white",
-                linewidth = lw,
-                show.legend = TRUE) +
-      scale_fill_manual(
-        breaks = c("D0",
-                   "D1",
-                   "D2",
-                   "D3",
-                   "D4"),
-        values = list(
-          "None" = "gray90",
-          "D0" = "#ffff00",
-          "D1" = "#fcd37f",
-          "D2" = "#ffaa00",
-          "D3" = "#e60000",
-          "D4" = "#730000"
-        ),
-        drop = FALSE,
-        guide = 
-          guide_legend(title = NULL,
-                       nrow = 1,
-                       override.aes = list(size = 0))
-      ) + 
-      geom_segment(data = month_breaks, 
-                   mapping = aes(x = as.integer(Week) - 0.5, 
-                                 y = 0.5,
-                                 yend = 8 - as.integer(Day) + 0.5),
-                   color = "black",
-                   linewidth = lw - 0.3,
-                   lineend = "round") +
-      geom_segment(data = month_breaks, 
-                   mapping = aes(x = as.integer(Week) - 0.5, 
-                                 xend = as.integer(Week) + 0.5,
-                                 y = 8 - as.integer(Day) + 0.5),
-                   color = "black",
-                   linewidth = lw - 0.3,
-                   lineend = "round") +
-      geom_segment(data = month_breaks, 
-                   mapping = aes(x = as.integer(Week) + 0.5,
-                                 y = 8 - as.integer(Day) + 0.5,
-                                 yend = 7.5),
-                   color = "black",
-                   linewidth = lw - 0.3,
-                   lineend = "round") +
-      geom_rect(xmin = 0.5, xmax = 54.5, ymin = 0.5, ymax = 7.5,
-                color = "white",
-                fill = "transparent",
-                linewidth = lw) +
-      scale_x_discrete(breaks = lab_breaks,
-                       labels = month.abb,
-                       position = "bottom") +
-      scale_y_discrete(
-        name = "title",
-        limits = rev(
-          c("Sun", "Mon", "Tue",
-            "Wed", "Thu", "Fri",
-            "Sat")
+    years %>%
+      {
+        split(.,       
+              cut(., 
+                  breaks = seq(min(.), max(.) + width, by = width), 
+                  include.lowest = TRUE,
+                  right = FALSE)
         )
-      ) +
-      coord_equal(clip = 'off') +
-      # facet_wrap("Year", ncol = 1,
-      #            strip.position = "bottom") +
-      lemon::facet_rep_wrap("Year",
-                            ncol = 1,
-                            strip.position = "top",
-                            repeat.tick.labels = 'bottom') +
-      theme_void() +
-      theme(
-        # axis.text.y = element_text(hjust = 1,
-        #                            vjust = 0.5,
-        #                            size = rel(0.7)),
-        axis.text.y = element_blank(),
-        axis.text.x = element_text(#face = "bold",
-          hjust = 0.5,
-          vjust = 0,
-          size = rel(0.8)),
-        legend.position = "bottom",
-        legend.justification = "right",
-        legend.text.position = "bottom",
-        legend.text = element_text(face = "bold"),
-        strip.text = element_text(face = "bold",
-                                  hjust = 0,
-                                  vjust = 0.5,
-                                  size = rel(1.2)),
-        plot.margin = margin(r = 0.5, unit = "in")
+      } %>%
+      purrr::map(
+        function(years){
+          calendar_data %<>%
+            dplyr::filter(Year %in% years)
+          
+          month_breaks <-
+            calendar_data %>%
+            dplyr::filter(lubridate::mday(Date) == 1,
+                          Month != "Jan")
+          
+          # lab_breaks <-
+          #   calendar_data %>%
+          #   dplyr::filter(lubridate::mday(Date) == 1,
+          #                 Year == min(Year)) %>%
+          #   dplyr::mutate(Week = ifelse(wday != 1, as.integer(Week) + 1, Week),
+          #                 Week = ifelse(Month == "Jan", 1, Week)) %$%
+          #   Week
+          
+          lab_breaks <-
+            c(Jan = 3,
+              Feb = 7,
+              Mar = 11,
+              Apr = 15,
+              May = 20,
+              Jun = 24,
+              Jul = 29,
+              Aug = 33,
+              Sep = 38,
+              Oct = 42,
+              Nov = 46,
+              Dec = 51
+            )
+          
+          lw = 0.8
+          
+          grid::grid.newpage()
+          
+          p <- 
+            calendar_data %>%
+            # tail(20)
+            ggplot() +
+            geom_tile(aes(x = Week, 
+                          y = Day, 
+                          fill = `USDM Class`),
+                      color = "white",
+                      linewidth = lw,
+                      show.legend = TRUE) +
+            scale_fill_manual(
+              breaks = c("D0",
+                         "D1",
+                         "D2",
+                         "D3",
+                         "D4"),
+              values = list(
+                "None" = "gray90",
+                "D0" = "#ffff00",
+                "D1" = "#fcd37f",
+                "D2" = "#ffaa00",
+                "D3" = "#e60000",
+                "D4" = "#730000"
+              ),
+              drop = FALSE,
+              guide = 
+                guide_legend(title = NULL,
+                             nrow = 1,
+                             override.aes = list(size = 0))
+            ) + 
+            geom_segment(data = month_breaks, 
+                         mapping = aes(x = as.integer(Week) - 0.5, 
+                                       y = 0.5,
+                                       yend = 8 - as.integer(Day) + 0.5),
+                         color = "black",
+                         linewidth = lw - 0.3,
+                         lineend = "round") +
+            geom_segment(data = month_breaks, 
+                         mapping = aes(x = as.integer(Week) - 0.5, 
+                                       xend = as.integer(Week) + 0.5,
+                                       y = 8 - as.integer(Day) + 0.5),
+                         color = "black",
+                         linewidth = lw - 0.3,
+                         lineend = "round") +
+            geom_segment(data = month_breaks, 
+                         mapping = aes(x = as.integer(Week) + 0.5,
+                                       y = 8 - as.integer(Day) + 0.5,
+                                       yend = 7.5),
+                         color = "black",
+                         linewidth = lw - 0.3,
+                         lineend = "round") +
+            geom_rect(xmin = 0.5, xmax = 54.5, ymin = 0.5, ymax = 7.5,
+                      color = "white",
+                      fill = "transparent",
+                      linewidth = lw) +
+            scale_x_discrete(breaks = lab_breaks,
+                             limits = 
+                               factor(as.character(1:54),
+                                      levels = 1:54, 
+                                      labels = 1:54, 
+                                      ordered = TRUE),
+                             labels = month.abb,
+                             position = "bottom",
+                             expand = expansion(0,0),
+                             drop = FALSE) +
+            scale_y_discrete(
+              name = "title",
+              limits = rev(
+                c("Sun", "Mon", "Tue",
+                  "Wed", "Thu", "Fri",
+                  "Sat")
+              ),
+              expand = expansion(0,0),
+              drop = FALSE
+            ) +
+            coord_equal(clip = 'off') +
+            # facet_wrap("Year", ncol = 1,
+            #            strip.position = "bottom") +
+            lemon::facet_rep_wrap("Year",
+                                  ncol = 1,
+                                  strip.position = "top",
+                                  repeat.tick.labels = 'bottom') +
+            theme_void() +
+            theme(
+              # axis.text.y = element_text(hjust = 1,
+              #                            vjust = 0.5,
+              #                            size = rel(0.7)),
+              axis.text.y = element_blank(),
+              axis.text.x = element_text(#face = "bold",
+                hjust = 0.5,
+                vjust = 0,
+                size = rel(0.8)),
+              legend.position = "bottom",
+              legend.justification = "right",
+              legend.text.position = "bottom",
+              legend.text = element_text(face = "bold"),
+              strip.text = element_text(face = "bold",
+                                        hjust = 0,
+                                        vjust = 0.5,
+                                        size = rel(1.2)),
+              plot.margin = margin(r = 0.5, unit = "in")
+            )
+          
+          
+          p %<>%
+            ggplotGrob()
+          
+          left_axis <-
+            p$layout %>%
+            tibble::as_tibble() %>%
+            dplyr::filter(name == "axis-l-1-1") %$%
+            l
+          
+          right_axis <-
+            p$layout %>%
+            tibble::as_tibble() %>%
+            dplyr::filter(name == "axis-r-1-1") %$%
+            r
+          
+          guide_t <- 
+            p$layout %>%
+            tibble::as_tibble() %>%
+            dplyr::filter(name == "guide-box-bottom") %$%
+            t
+          
+          guide_l <- 
+            p$layout %>%
+            tibble::as_tibble() %>%
+            dplyr::filter(name == "guide-box-bottom") %$%
+            l
+          
+          p$widths[left_axis] <- unit(0.5, "in")
+          # p$widths[right_axis] <- unit(0.5, "in")
+          
+          p %>%
+            gtable::gtable_add_grob(
+              grid::textGrob(
+                label =
+                  "US Drought Monitor",
+                just = c(0,0),
+                x = unit(0, "npc"),
+                # y = unit(guide_top * 9, "in"),
+                y = unit(0.6, "npc"),
+                gp = grid::gpar(fontface = "bold",
+                                fontsize = 18)
+              ),
+              t = guide_t,
+              l = guide_l,
+              name = "product",
+              clip = "off"
+            ) %>%
+            gtable::gtable_add_grob(
+              grid::textGrob(
+                label =
+                  county_names %>%
+                  dplyr::filter(FSA_CODE == county) %$%
+                  paste0(County," County, ", State),
+                just = c(0,0),
+                x = unit(0, "npc"),
+                # y = unit(guide_top * 9, "in"),
+                y = unit(0.1, "npc"),
+                gp = grid::gpar(fontface = "plain",
+                                fontsize = 14)
+              ),
+              t = guide_t,
+              l = guide_l,
+              name = "county",
+              clip = "off"
+            ) %>%
+            grid::grid.draw()
+        }
       )
-    
-    
-    p %<>%
-      ggplotGrob()
-    
-    left_axis <-
-      p$layout %>%
-      tibble::as_tibble() %>%
-      dplyr::filter(name == "axis-l-1-1") %$%
-      l
-    
-    right_axis <-
-      p$layout %>%
-      tibble::as_tibble() %>%
-      dplyr::filter(name == "axis-r-1-1") %$%
-      r
-    
-    guide_t <- 
-      p$layout %>%
-      tibble::as_tibble() %>%
-      dplyr::filter(name == "guide-box-bottom") %$%
-      t
-    
-    guide_l <- 
-      p$layout %>%
-      tibble::as_tibble() %>%
-      dplyr::filter(name == "guide-box-bottom") %$%
-      l
-    
-    p$widths[left_axis] <- unit(0.5, "in")
-    # p$widths[right_axis] <- unit(0.5, "in")
-    
-    p %>%
-      gtable::gtable_add_grob(
-        grid::textGrob(
-          label =
-            "US Drought Monitor",
-          just = c(0,0),
-          x = unit(0, "npc"),
-          # y = unit(guide_top * 9, "in"),
-          y = unit(0.6, "npc"),
-          gp = grid::gpar(fontface = "bold",
-                          fontsize = 18)
-        ),
-        t = guide_t,
-        l = guide_l,
-        name = "product",
-        clip = "off"
-      ) %>%
-      gtable::gtable_add_grob(
-        grid::textGrob(
-          label =
-            county_names %>%
-            dplyr::filter(FSA_CODE == county) %$%
-            paste0(County," County, ", State),
-          just = c(0,0),
-          x = unit(0, "npc"),
-          # y = unit(guide_top * 9, "in"),
-          y = unit(0.1, "npc"),
-          gp = grid::gpar(fontface = "plain",
-                          fontsize = 14)
-        ),
-        t = guide_t,
-        l = guide_l,
-        name = "county",
-        clip = "off"
-      ) %>%
-      grid::grid.draw()
   }
 
-grid::grid.newpage()
-county_names %>%
-  dplyr::filter(County == "Missoula",
-                State == "Montana") %$%
-  county_drought_calendar(FSA_CODE, 2017:2024) %>%
-  ggsave(plot = .,
-         filename = "Missoula_MT.pdf",
-         device = cairo_pdf,
-         width = 7.5,
-         height = 10,
-         bg = "white",
-         dpi = 600)
 
+plot_county <- 
+  function(county, state, years = 2000:2024){
+    cairo_pdf( filename = paste0(county, "_", state, ".pdf"),
+               width = 10,
+               height = 7.5,
+               bg = "white",
+               fallback_resolution = 600)
+    county_names %>%
+      dplyr::filter(County == county,
+                    State == state) %$%
+      county_drought_calendar(FSA_CODE,
+                              years = years, 
+                              width = 4)
+    dev.off()
+    
+  }
+
+plot_county("Missoula", "Montana")
+plot_county("Powell", "Montana")
+plot_county("Flathead", "Montana")
+plot_county("Roosevelt", "Montana")
+plot_county("Boise", "Idaho")
 
